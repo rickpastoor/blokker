bdbf73f552b96055793666b4c3acbfa4133f371d
Rick Pastoor <rick@blendle.com>
2017-05-13 18:43:45 +0200

    Add ugly interval to make sure you won’t be able to get around

--- src/blokker.js
+++ src/blokker.js
@@ -1,17 +1,31 @@
-// Twitter
-if (window.location == 'https://twitter.com/') {
-  var streamContainer = document.querySelector('.stream-container');
-  streamContainer.parentNode.removeChild(streamContainer);
+function blockStuff() {
+  // Twitter
+  if (window.location == 'https://twitter.com/') {
+    var streamContainer = document.querySelector('.stream-container');
+    if (streamContainer) {
+      streamContainer.parentNode.removeChild(streamContainer);
+    }
+  }
+
+  // YouTube
+  if (window.location == 'https://www.youtube.com/') {
+    var mainFeed = document.querySelector('#feed-main-what_to_watch');
+    if (mainFeed) {
+      mainFeed.parentNode.removeChild(mainFeed);
+    }
+  }
+
+  // Facebook
+  if (window.location == 'https://www.facebook.com/') {
+    var streamPagelet = document.querySelector('#stream_pagelet');
+    if (streamPagelet) {
+      streamPagelet.parentNode.removeChild(streamPagelet);
+    }
+  }
 }
 
-// YouTube
-if (window.location == 'https://www.youtube.com/') {
-  var mainFeed = document.querySelector('#feed-main-what_to_watch');
-  mainFeed.parentNode.removeChild(mainFeed);
-}
+var retryLoop = setInterval(function () {
+  blockStuff();
+}, 1000);
 
-// Facebook
-if (window.location == 'https://www.facebook.com/') {
-  var streamPagelet = document.querySelector('#stream_pagelet');
-  streamPagelet.parentNode.removeChild(streamPagelet);
-}
+blockStuff();
