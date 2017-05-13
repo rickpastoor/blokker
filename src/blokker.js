eb0b1c14cc010c52e8e96f486e7e0ef7540a8014
Rick Pastoor <rick@blendle.com>
2017-05-13 20:12:16 +0200

    Way simpler and better solution

--- src/blokker.js
+++ src/blokker.js
@@ -1,20 +1,4 @@
 function blockStuff() {
-  // Twitter
-  if (window.location == 'https://twitter.com/') {
-    var streamContainer = document.querySelector('.stream-container');
-    if (streamContainer) {
-      streamContainer.parentNode.removeChild(streamContainer);
-    }
-  }
-
-  // YouTube
-  if (window.location == 'https://www.youtube.com/') {
-    var mainFeed = document.querySelector('#feed-main-what_to_watch');
-    if (mainFeed) {
-      mainFeed.parentNode.removeChild(mainFeed);
-    }
-  }
-
   // Facebook
   if (window.location == 'https://www.facebook.com/') {
     var streamPagelet = document.querySelector('#stream_pagelet');
