1e090142f3a261a870e0a1cfb727a0def552bc03
Rick Pastoor <rick@blendle.com>
2017-05-16 19:56:20 +0200

    Add safari support

---
+++ tools/make-safari.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This script assumes a linux environment
+
+source ./config
+
+echo "*** blokker.safari: Copying files"
+
+DES=dist/build/blokker.safariextension
+rm -rf $DES
+mkdir -p $DES
+
+cp -R src/*                             $DES/
+cp    platform/safari/Info.plist        $DES/
+
+# Replace version
+sed -i.bak 's/BLOKKER_VERSION/'$BLOKKER_VERSION'/g' $DES/Info.plist
+rm $DES/Info.plist.bak
+
+if [ "$1" = all ]; then
+    echo "*** blokker.safari: Creating package..."
+    pushd $(dirname $DES/)
+    xarjs create blokker.safariextz --cert ./../../platform/safari/cert.pem --cert ./../../platform/safari/apple-intermediate.pem --cert ./../../platform/safari/apple-root.pem --private-key ./../../platform/safari/privatekey.pem $(basename $DES/)
+fi
+
+echo "*** scrummer.safari: Package done."
